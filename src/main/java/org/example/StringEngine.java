1f279d8514ce2c7a3151333ccdaa4b45177e6e2b
Riterium Time <riteriumtime@gmail.com>
2022-11-09 11:08:52 +0300

    Initial commit

---
+++ src/main/java/org/example/StringEngine.java
@@ -0,0 +1,24 @@
+package org.example;
+public class StringEngine {
+    public static String concatString(String in, String add)
+    {
+        if (in != null)
+            in += add;
+        return in;
+    }
+
+    public static String multiplicateString(String str, int n)
+    {
+        if (str != null && n > 0) {
+            StringBuilder strBuild = new StringBuilder(str);
+            for (int i = 1; i < n; i++) {
+                strBuild.append(str);
+            }
+            return strBuild.toString();
+        }
+        else
+        {
+            return new String();
+        }
+    }
+}
---
+++ src/test/java/org/example/StringEngineTest.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringEngineTest {
+
+    @Test
+    @DisplayName("Concat Strings")
+    void concatString() {
+        String str = new String();
+        assertAll(() -> assertEquals("random str", StringEngine.concatString("random str", "")),
+                () -> assertEquals("random str", StringEngine.concatString( "", "random str")),
+                () -> assertEquals("random str random str", StringEngine.concatString("random str ", "random str")),
+                () -> assertEquals(str, StringEngine.concatString(str, str)));
+    }
+
+    @Test
+    @DisplayName("Multiplicate String n times")
+    void multiplicateString() {
+        assertAll(() -> assertEquals("", StringEngine.multiplicateString("random str", 0)),
+                () -> assertEquals("rand", StringEngine.multiplicateString( "rand", 1)),
+                () -> assertEquals("randrandrand", StringEngine.multiplicateString("rand", 3)),
+                () -> assertEquals("rand rand rand rand rand rand rand rand rand ", StringEngine.multiplicateString("rand ", 9)));
+    }
+}
